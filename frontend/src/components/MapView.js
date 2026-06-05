f0c15da1866daadccfb24801357c5ea6ee3ad8f2
yohan mathew <yohan25110364@snuchennai.edu.in>
2026-06-05 14:47:58 +0530

    Update MapView.js

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import socket from '../services/socket';
